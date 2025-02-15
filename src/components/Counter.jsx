3e8d439126de2fd862bdce775680813a409de410
MOHMMED.MOHIUDDIN <149958768+MDmohiuddin-web@users.noreply.github.com>
2025-02-16 01:02:10 +0600

    working

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../redux/featches/counter/CounterSlice";
+import { decrement, decrementByAmount, increment, incrementByAmount } from "../redux/featches/counter/CounterSlice";
 
 const Counter = () => {
-    const count = useSelector(state => state.Counter.value)
-    const dispatch = useDispatch()
+    const count = useSelector(state => state.Counter.value);
+    const dispatch = useDispatch();
 
     return (
-        <div className="flex flex-row gap-5 items-center justify-center h-screen w-1/2 mx-auto  text-2xl bg-gray-200">
-            <button className="px-3 py-5 bg-yellow-500" onClick={() => dispatch(decrement())}>Decrement -</button>
+        <div className="flex flex-row gap-5 items-center justify-center h-screen w-1/2 mx-auto text-2xl bg-gray-200">
+            <button className="px-3 py-5 bg-yellow-500" onClick={() => dispatch(decrementByAmount(5))}>
+                Decrement - 5
+            </button>
+            <button className="px-3 py-5 bg-yellow-500" onClick={() => dispatch(decrement())}>
+                Decrement -
+            </button>
             <h1>{count}</h1>
-            <button className="px-3 py-5 bg-orange-500" onClick={() => dispatch(increment())}>Increment +</button>
-
-
-
-
+            <button className="px-3 py-5 bg-orange-500" onClick={() => dispatch(increment())}>
+                Increment +
+            </button>
+            <button className="px-3 py-5 bg-blue-500 text-white" onClick={() => dispatch(incrementByAmount(5))}>
+                Increment + 5
+            </button>
         </div>
     );
 };
 
 export default Counter;
+
